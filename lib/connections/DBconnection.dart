921f975c625b487c49f40d3ed896c68e63b23495
rajputchauhan1 <prathamchauhan2001@gmail.com>
2022-09-03 12:31:08 +0530

    Added class  Dbconnection

--- lib/connections/DBconnection.dart
+++ lib/connections/DBconnection.dart
@@ -1,10 +1,36 @@
-import 'package:mongo_dart/mongo_dart.dart' ;
+import 'package:mongo_dart/mongo_dart.dart' show Db, DbCollection;
+class DBConnection {
 
-class MongoDatabase{
-  static Db db;
-  static connect() async {
-    const String MONGO_CONN_URL = "mongodb+srv://user:12345@attendance.kobg93v.mongodb.net/?retryWrites=true&w=majority";
-    db = await Db.create(MONGO_CONN_URL);
-    await db.open();
-}
+  static late DBConnection instance;
+
+  final String _host = "DATABASE SERVER";
+  final String _port = "DATABASE PORT";
+  final String _dbName = "DATABASE NAME";
+  late Db db;
+
+  static getInstance(){
+    instance ??= DBConnection();
+    return instance;
+  }
+
+  Future<Db> getConnection() async{
+    if (db == null){
+      try {
+        db = Db(_getConnectionString());
+        await db.open();
+      } catch(e){
+        print(e);
+      }
+    }
+    return db;
+  }
+
+  _getConnectionString(){
+    return "mongodb+srv://user:12345@attendance.kobg93v.mongodb.net/?retryWrites=true&w=majority";
+  }
+
+  closeConnection() {
+    db.close();
+  }
+
 }
